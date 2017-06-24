35f01a0d542e826b868ca873c1c2e4f23c4697a6
Alexander Davis <akonidavis@gmail.com>
2017-06-24 12:23:05 -1000

    reverse words added

---
+++ reverse_words.js
@@ -0,0 +1,22 @@
+// reverse words
+// Question: How would you reverse words in a sentence?
+
+// function reverseWords(str){
+//  var rev = [],
+//      wordLen = 0;
+//  for(var i = str.length-1; i>=0; i--){
+//    if(str[i]==' ' || i==0){
+//      rev.push(str.substr(i,wordLen+1));
+//      wordLen = 0;
+//    }
+//    else
+//      wordLen++;
+//  }
+//  return rev.join(' ');
+// }
+
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
+console.log(reverseWords('Who are you going to call?'));
