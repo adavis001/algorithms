5a15659714f22779100ce90448dd4f150566d9c2
Alexander Davis <akonidavis@gmail.com>
2017-06-26 00:21:55 -1000

    added another way to do palindromes

--- check_palindrome.js
+++ check_palindrome.js
@@ -1,15 +1,36 @@
 // check palindrome
 // Question: How will you verify a word as palindrome?
+// Palindrom is spelled the same if it is reversed.
+
+// function isPalindrome(str){
+// var reverseStr = str.split('').reverse().join('');
+
+//   if (reverseStr === str){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
 
 function isPalindrome(str){
-var reverseStr = str.split('').reverse().join('');
+var reverseStr = [],
+    tempStr = str.split('');
+// .reverse().join('');
 
-  if (reverseStr === str){
+
+for (var i = tempStr.length - 1; i >= 0; i--){
+  reverseStr.push(tempStr[i]);
+}
+
+  if (reverseStr.join('') === str){
     return true;
   } else {
     return false;
   }
 }
+
 
 console.log(isPalindrome('tacocat'));
 console.log(isPalindrome('cat'));
